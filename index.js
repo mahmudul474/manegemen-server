1539674e8af7cc45237bc3ef441535f642921588
devSobuj910 <ftsobuj222@gmail.com>
2022-12-18 04:18:40 +0600

    create ordre delet order uptgrade oder complete

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
  const  app= express();
  const cors=require("cors")
  const port=process.env.PORT || 5000
-
+ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
  require('dotenv').config()
 
 
@@ -27,7 +27,7 @@ app.use(cors())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@474.79d3jxt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -39,8 +39,88 @@ try{
   const productcollection=client.db("carDeller").collection("products")
 
 
+  const orderscollection=client.db("carDeller").collection("orders")
+
+
  
-  
+//  load all products
+
+app.get("/cars",async(req,res)=>{
+
+   const query={};
+  const result=await productcollection.find(query).toArray()
+  res.send(result)
+})
+
+
+app.get("/cars/:id",async(req,res)=>{
+ const id=req.params.id
+ const query={_id: ObjectId(id)}
+ const result=await productcollection.findOne(query)
+ console.log(result)
+ res.send(result)
+
+})
+
+
+
+
+//order post
+app.post("/orders",async(req,res)=>{
+  const order=req.body;
+  const result=await  orderscollection.insertOne(order)
+  console.log(result)
+  res.send(result)
+})
+
+app.get("/orders",async(req,res)=>{
+let query={}
+if(req.query.email){
+  query={  
+    email:req.query.email
+  }
+}
+const result=await orderscollection.find(query).toArray()
+res.send(result)
+
+
+
+})
+
+
+app.delete("/orders/:id",async(req,res)=>{
+  const id=req.params.id
+  const query={_id: ObjectId(id)}
+  const result=await orderscollection.deleteOne(query)
+  console.log(result)
+  res.send(result)
+})
+
+
+
+app.patch("/orders/:id", async (req, res) => {
+  const id=req.params.id
+   const query={_id: ObjectId(id)}
+   const status=req.body.status
+   const uptdoc={
+    $set:{
+      status:status
+    }
+   }
+   const result=await orderscollection.updateOne(query,uptdoc)
+   console.log(result)
+   res.send(result)
+})
+
+
+
+
+
+
+
+
+
+
 }
 finally{}
 
